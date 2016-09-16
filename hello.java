b46a4c09b26532b372d7919312c215a93da359fb
nivedharu <Nivethadharani96@gmail.com>
2016-09-16 09:33:01 +0530

    Update and rename .gitignore to hello.java

---
+++ hello.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class hello
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+char[] k=s.toCharArray();
+int i;
+int count;
+for(i=0;i<k.length;i++)
+{
+if(a[i]==a[i+1])
+{
+count++;
+}
+}
+System.out.println(count);
+}
+}
